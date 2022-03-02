c7812df21369be167097c42c35a32f6535405063
RzTutul <rz.tutul@gmail.com>
2022-03-02 11:49:27 +0600

    UI Complete

---
+++ app/src/main/java/com/example/quizapp/HomeFrag.java
@@ -0,0 +1,45 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quizapp.databinding.FragmentHomeBinding;
+
+
+public class HomeFrag extends Fragment {
+
+
+    FragmentHomeBinding binding;
+
+    public HomeFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        // Inflate the layout for this fragment
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
